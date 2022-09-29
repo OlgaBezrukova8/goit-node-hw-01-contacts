2eb7c96ebe2fe7c60a3c0e6ddf7d3742371460bf
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-09-29 20:12:53 +0100

    Added some actions to contacts

--- contacts.js
+++ contacts.js
@@ -1,2 +1,25 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs").promises;
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath, "utf-8");
+  console.log(result);
+};
+
+const getContactById = async (contactId) => {};
+
+const removeContact = async (contactId) => {};
+
+const addContact = async (name, email, phone) => {
+    const data = await fs.appendFile(contactsPath, { name, email, phone });
+    console.log(data)
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
